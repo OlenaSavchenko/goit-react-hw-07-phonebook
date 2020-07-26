dd337dc12ebdaf2c2980a754812f4978f6e5eb12
Olena Savchenko <olsavchenko1@gmail.com>
2020-07-26 12:21:32 +0300

    Updates

---
+++ precache-manifest.8c74def9cce1ed5f855e58da669b38c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bafe3dc51a2ec90af27d462523d7152d",
+    "url": "/goit-react-hw-07-phonebook/index.html"
+  },
+  {
+    "revision": "66fc7b98668f5b0fe846",
+    "url": "/goit-react-hw-07-phonebook/static/css/2.4b502e0b.chunk.css"
+  },
+  {
+    "revision": "23488f38a5d87870bc83",
+    "url": "/goit-react-hw-07-phonebook/static/css/main.279799ee.chunk.css"
+  },
+  {
+    "revision": "66fc7b98668f5b0fe846",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.f98c7ae8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.f98c7ae8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23488f38a5d87870bc83",
+    "url": "/goit-react-hw-07-phonebook/static/js/main.02843807.chunk.js"
+  },
+  {
+    "revision": "08918a230102c367f62e",
+    "url": "/goit-react-hw-07-phonebook/static/js/runtime-main.ad8b8cc9.js"
+  }
+]);
